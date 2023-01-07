dc7583eee9f0248d87dbbbe573e7e4b88e7f4435
Kevin Wang <wy721@qq.com>
2023-01-07 03:59:05 +0000

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
     let rootdir = std::env::var("CARGO_MANIFEST_DIR").expect("Missing CARGO_MANIFEST_DIR");
     let target = std::env::var("TARGET").unwrap();
-    let is_pink = target.starts_with("wasm32"); // == "wasm32-unknown-unknown";
-    if is_pink {
+    let is_wasm32 = target.starts_with("wasm32"); // == "wasm32-unknown-unknown";
+    if is_wasm32 {
         let status = std::process::Command::new("make")
             .arg("libc")
             .spawn()
@@ -33,28 +33,31 @@ fn main() {
         cc.file(file);
     }
     cc.flag("-funsigned-char")
-        .flag("-w")
-        .define("CONFIG_BIGNUM", None);
+        .define("CONFIG_BIGNUM", None)
+        .define("_GNU_SOURCE", None)
+        .define("__pink__", "1")
+        .flag("-w");
 
-    if is_pink {
-        cc.define("__pink__", "1");
-        cc.define("_GNU_SOURCE", None);
+    if is_wasm32 {
         cc.include("pink-libc/sysroot/include");
         cc.archiver("llvm-ar");
         cc.warnings(false);
     }
     cc.compile("qjs");
 
-    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed=csrc/qjs-pink.h");
     let mut builder = bindgen::Builder::default()
         .header("csrc/qjs-pink.h")
+        .clang_arg("-funsigned-char")
+        .clang_arg("-DCONFIG_BIGNUM")
+        .clang_arg("-D_GNU_SOURCE")
+        .clang_arg("-D__pink__=1")
         .clang_arg("-w")
         .use_core()
         .parse_callbacks(Box::new(bindgen::CargoCallbacks));
-    if target.starts_with("wasm32") {
+    if is_wasm32 {
         builder = builder
             .clang_arg("-fvisibility=default")
-            .clang_arg("-D__pink__=1")
             .clang_arg(format!("-I{}/pink-libc/sysroot/include", rootdir));
     }
     let bindings = builder.generate().expect("Unable to generate bindings");
