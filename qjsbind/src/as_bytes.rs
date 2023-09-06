b9502f4cadb5c0b549a7e768542c0397a7a3fe74
Kevin Wang <wy721@qq.com>
2023-09-06 08:57:26 +0000

    Lose bound of as_bytes to TryFrom<Vec<u8>>

--- qjsbind/src/as_bytes.rs
+++ qjsbind/src/as_bytes.rs
@@ -2,7 +2,7 @@ use core::ptr::NonNull;
 
 use alloc::vec::Vec;
 
-use super::{c, Result, Value};
+use super::{c, Error, Result, Value};
 
 pub fn encode_as_bytes<T: AsRef<[u8]>>(ctx: NonNull<c::JSContext>, data: &T) -> Result<Value> {
     Ok(Value::from_bytes(ctx, data.as_ref()))
@@ -10,8 +10,8 @@ pub fn encode_as_bytes<T: AsRef<[u8]>>(ctx: NonNull<c::JSContext>, data: &T) -> 
 
 pub fn decode_as_bytes<T>(js_value: Value) -> Result<T>
 where
-    Vec<u8>: Into<T>,
+    Vec<u8>: TryInto<T>,
 {
     let bytes = js_value.decode_bytes()?;
-    Ok(bytes.into())
+    Ok(bytes.try_into().or(Err(Error::Expect("try from bytes")))?)
 }
