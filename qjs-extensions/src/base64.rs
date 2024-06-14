954c324404cee0c126b7ba615362b360b58e4b8f
Kevin Wang <wy721@qq.com>
2024-06-14 10:31:46 +0800

    Export base64 api

--- qjs-extensions/src/base64.rs
+++ qjs-extensions/src/base64.rs
@@ -4,23 +4,30 @@ use js::{AsBytes, BytesOrString, ErrorContext, JsString, Result};
 
 #[js::host_call]
 pub fn encode(data: BytesOrString, pad: bool) -> String {
+    b64_encode(data, pad)
+}
+
+pub fn b64_encode<T: AsRef<[u8]>>(data: T, pad: bool) -> String {
     if pad {
-        general_purpose::STANDARD.encode(data.as_bytes())
+        general_purpose::STANDARD.encode(data)
     } else {
-        general_purpose::STANDARD_NO_PAD.encode(data.as_bytes())
+        general_purpose::STANDARD_NO_PAD.encode(data)
     }
 }
+
 #[js::host_call]
 pub fn decode(base64_str: JsString, pad: bool) -> Result<AsBytes<Vec<u8>>> {
+    b64_decode(base64_str.as_str(), pad).map(AsBytes)
+}
+
+pub fn b64_decode<T: AsRef<[u8]>>(encoded: T, pad: bool) -> Result<Vec<u8>> {
     if pad {
         general_purpose::STANDARD
-            .decode(base64_str.as_str())
-            .map(AsBytes)
+            .decode(encoded)
             .context("invalid base64 string")
     } else {
         general_purpose::STANDARD_NO_PAD
-            .decode(base64_str.as_str())
-            .map(AsBytes)
+            .decode(encoded)
             .context("invalid base64 string")
     }
 }
